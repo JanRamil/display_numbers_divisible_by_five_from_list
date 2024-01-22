96d976bb020c98c8ca962ebd7871560ad472ed88
JAN RAMIL ELIZER ALARCON <janramila@gmail.com>
2024-01-23 01:38:25 +0800

    Creating the code that will check if the numbers are divisible by five

--- display_numbers_divisible_by_5_from_list.py
+++ display_numbers_divisible_by_5_from_list.py
@@ -6,5 +6,11 @@ number_list = [10, 20, 33, 46, 55]
 
 # Creating code that will print the numbers that are divisible by five
 print("Given list: ", number_list)
-print("Numbers that are divisible by five are: ",)
+print("Numbers that are divisible by five are: ")
+
+# Creating the code that will check if the numbers are divisible by five
+for number in number_list:
+    if number % 5 == 0:
+        # Create a code that will print the number
+        print(number)
 
