436005e16f25f9ab900297ef959b3ec97ec5a5c3
JAN RAMIL ELIZER ALARCON <janramila@gmail.com>
2024-01-23 01:35:27 +0800

    Creating the starting code for the number list

--- display_numbers_divisible_by_5_from_list.py
+++ display_numbers_divisible_by_5_from_list.py
@@ -2,4 +2,9 @@
 # Exercise 6
 
 # Creating pseudocode
+number_list = [10, 20, 33, 46, 55]
+
+# Creating code that will print the numbers that are divisible by five
+print("Given list: ", number_list)
+print("Numbers that are divisible by five are: ",)
 
